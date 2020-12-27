11a871f328925104a24cf3ad5cccfd7c9c410ff1
vrp <vinayrpatel0000@gmail.com>
2020-12-27 12:57:12 +0530

    calendar update

--- containers/python/calendar-app.py
+++ containers/python/calendar-app.py
@@ -2,9 +2,4 @@ import calendar
 
 print('Welcome to the Calendar application!')
 
-year = int(input('Please enter any year:'))
-month = int(input('Please enter any month number:'))
-
-print(calendar.month(year, month))
-
-print('Have a nice day!')
+import calendar as cal
