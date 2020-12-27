c4e278ddd232dd8a4f2a1c8c1aa994c345fb456d
vrp <vinayrpatel0000@gmail.com>
2020-12-27 12:59:10 +0530

    calendar update

--- containers/python/calendar-app.py
+++ containers/python/calendar-app.py
@@ -3,3 +3,13 @@ import calendar
 print('Welcome to the Calendar application!')
 
 import calendar as cal
+yr = int(input ("Please enter any year: "))
+mon = int(input ("Please enter any month: "))
+while 1 == 1:
+    if (mon >=1 and mon <=12 ):
+        print(cal.month(yr,mon))
+        print ("Have a nice day!")
+        break
+    else:
+        print("Incorrect value of month entered. TRY Again")
+        mon = int(input ("Please enter any month: "))
